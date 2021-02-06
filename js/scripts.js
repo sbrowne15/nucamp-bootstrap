f2c451274d01add30a9adcabd8cd2393f68d8351
sbrowne15@gmail.com <sbrowne15@gmail.com>
2021-02-06 11:31:02 -0600

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -10,5 +10,17 @@ $(function() {
             $("#carouselButton").children("i").removeClass("fa-play");
             $("#carouselButton").children("i").addClass("fa-pause");
         }
-    });           
+    });
+});
+
+$(function() {
+    $("#reserveButton").click(function(){
+        $('#reserveModal').modal("show")
+    });
+});
+
+$(function() {
+    $("#loginButton").click(function(){
+        $('#loginModal').modal("show")
+    });
 });
